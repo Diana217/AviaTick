4f4c29c9ad1ba2c51170b110c4f92756eadf00c8
Diana217 <dulko.diana.2014@gmail.com>
2022-11-09 18:29:42 +0200

    change

--- AviaTick/Program.cs
+++ AviaTick/Program.cs
@@ -13,6 +13,7 @@ builder.Services.AddDbContext<AviaTickDbContext>(option => option.UseSqlServer(
 var app = builder.Build();
 
 
+
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
